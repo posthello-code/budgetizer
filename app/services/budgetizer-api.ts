9f8626cb017bb8281dacb4155dced321fba6158a
posthello-code <github@alexjensen.net>
2024-07-30 12:33:59 -0700

    rename file

--- app/services/budgetizer-api.ts
+++ app/services/budgetizer-api.ts
@@ -3,20 +3,14 @@ import environment from "../environment/default";
 export default class budgetizerApi {
   static async createBudget(data: {}): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets`;
-    return axios.post(url, data).then((res) => {
-      return res.data;
-    });
+    return axios.post(url, data);
   }
   static async getBudgetById(id: string): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets/${id}`;
-    return axios.get(url).then((res) => {
-      return res.data;
-    });
+    return axios.get(url);
   }
-  static async updateBudgetById(id: string, data: {}): Promise<AxiosResponse> {
+  static async updateBudgetById(id: string, data: any): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets/${id}`;
-    return axios.put(url, data).then((res) => {
-      return res.data;
-    });
+    return axios.put(url, data);
   }
 }
