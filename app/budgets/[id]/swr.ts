38b228903ba520747446b0e50a1aec5b100f7651
posthello-code <github@alexjensen.net>
2024-07-30 12:34:22 -0700

    rename api file

--- app/budgets/[id]/swr.ts
+++ app/budgets/[id]/swr.ts
@@ -1,9 +1,10 @@
 import { AxiosResponse } from "axios";
 import useSWR from "swr";
-import budgetizerApi from "@/app/services/api";
+import budgetizerApi from "../../services/budgetizer-api";
 
-const fetcher = (id: string): Promise<AxiosResponse> => {
-  return budgetizerApi.getBudgetById(id);
+const fetcher = async (id: string): Promise<AxiosResponse> => {
+  const r = await budgetizerApi.getBudgetById(id);
+  return r.data;
 };
 
 export default function useBudget(id: String) {
