50a6904f93f9f2d7cd1142ca56fe4d54db29ad99
posthello-code <github@alexjensen.net>
2024-07-30 00:57:48 -0700

    remainder of server interactions

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,34 +1,74 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
 import { Controls } from "./controls";
 import { BudgetPie } from "./budget-pie";
 import Link from "next/link";
+import axios from "axios";
+import useBudget from "./[id]/budgetData";
 
-export default function Home() {
+export default function BudgetPage(options: any) {
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+  const [loadedOnce, setLoadedOnce] = useState<boolean>(false);
+  const { data, isLoading, isError } = useBudget(options.searchParams.id);
   const [monthlyIncome, setMonthlyIncome] = useState<number>(1000.0);
   const [expenses, setExpenses] = useState<
     [{ id: String; value: number; label: String }] | []
   >([]);
-  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(true);
+  let [isSmallScreen, setIsSmallScreen] = useState<boolean>(true);
 
   const onResize = () => {
     setIsSmallScreen(window.innerWidth < 1024);
   };
 
   useEffect(() => {
+    // only load the data once to avoid race condition
+    if (!loadedOnce && data && data?.data.monthlyIncome !== monthlyIncome) {
+      setMonthlyIncome(data.data.monthlyIncome);
+    }
+    if (!loadedOnce && data && data?.data.expenses !== expenses) {
+      setExpenses(data.data.expenses);
+      setLoadedOnce(true);
+    }
+
     window.addEventListener("resize", onResize);
     onResize();
   });
 
+  if (options.searchParams.id) {
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error...</div>;
+  }
+
   return (
     <main style={{ height: "100vh" }}>
-      {
-        // TODO: uncomment once data loading can be done using budget name and passphrase
-        /* <button className="p-1">
-        <Link href="/">Back</Link>
-      </button> */
-      }
+      <div id="budget-top-buttons" className="p-1">
+        {isSaving && <div>LOADING...</div>}
+        <button className="btn-primary">
+          <Link href="/">Back</Link>
+        </button>
+        <button
+          className="btn-primary"
+          onClick={async () => {
+            const budgetData = {
+              data: { monthlyIncome: monthlyIncome, expenses: expenses },
+            };
+            const req = axios.post(
+              "https://budgetizer.onrender.com/budgets",
+              budgetData,
+              { headers: { "Content-Type": "application/json" } }
+            );
+            setIsSaving(true);
+            const results = await Promise.resolve(req);
+            alert(
+              `To load your data in the future, enter the following ID on the home page ${results.data._id} `
+            );
+            setIsSaving(false);
+          }}
+        >
+          Save
+        </button>
+      </div>
+
       <div
         id="primary-view"
         style={{
@@ -36,6 +76,7 @@ export default function Home() {
           flex: 1,
           flexDirection: isSmallScreen ? "column" : "row",
           alignItems: "center",
+          justifyContent: "center",
           height: "100%",
         }}
       >
