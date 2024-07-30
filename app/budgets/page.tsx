4c0d99ab17e9e2ab68a797afea0abdfa76541d92
posthello-code <github@alexjensen.net>
2024-07-30 12:25:30 -0700

    update existing budget instead of always creating new ones

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from "react";
 import { Controls } from "./components/controls";
 import { BudgetPie } from "./components/budget-pie";
 import Link from "next/link";
-import axios from "axios";
 import useBudget from "./[id]/swr";
+import budgetizerApi from "../services/api";
+import { useRouter } from "next/navigation";
 
 export default function BudgetPage(options: any) {
+  const router = useRouter();
   const [isSaving, setIsSaving] = useState<boolean>(false);
   const [loadedOnce, setLoadedOnce] = useState<boolean>(false);
   const { data, isLoading, isError } = useBudget(options.searchParams.id);
@@ -57,17 +59,24 @@ export default function BudgetPage(options: any) {
             const budgetData = {
               data: { monthlyIncome: monthlyIncome, expenses: expenses },
             };
-            const req = axios.post(
-              "https://budgetizer.onrender.com/budgets",
-              budgetData,
-              { headers: { "Content-Type": "application/json" } }
-            );
             setIsSaving(true);
-            const results = await Promise.resolve(req);
-            alert(
-              `To load your data in the future, enter the following ID on the home page ${results.data._id} `
-            );
-            setIsSaving(false);
+            let results;
+            const alertText =
+              "To load your data in the future, enter the following ID on the home page";
+            if (options.searchParams.id) {
+              results = await budgetizerApi.updateBudgetById(
+                options.searchParams.id,
+                budgetData
+              );
+              setIsSaving(false);
+              console.log(results.data);
+              alert(`${alertText} ${options.searchParams.id}`);
+            } else {
+              results = await budgetizerApi.createBudget(budgetData);
+              setIsSaving(false);
+              alert(`${alertText} ${results.data._id}`);
+              router.push(`/budgets?id=${results.data._id}`);
+            }
           }}
         >
           Save
