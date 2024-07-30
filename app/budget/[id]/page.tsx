52b521430703ae6526204df4920c6c9b9e8cf99c
posthello-code <github@alexjensen.net>
2024-07-30 11:38:37 -0700

    rename files

--- app/budget/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-export default function Page(options: { params: { id: String } }) {
-  const router = useRouter();
-  router.push("/budget?id=" + options.params.id);
-}
