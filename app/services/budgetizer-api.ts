75031e29fe3b4ab5a534b29011eddcceb6cd5691
posthello-code <github@alexjensen.net>
2024-07-31 00:45:20 -0700

    added crypto to api calls (symetric key hard coded, will add user provided one later)

--- app/services/budgetizer-api.ts
+++ app/services/budgetizer-api.ts
@@ -1,33 +1,64 @@
 import axios, { AxiosResponse } from "axios";
 import environment from "../environment/default";
 import CryptoJS from "crypto-js";
+import { Budget, EncryptedBudget } from "../budgets/models";
 export default class budgetizerApi {
+  private static encryptBudgetData(
+    data: string,
+    passphrase = "super secret phrase"
+  ) {
+    const encryptedPayload = CryptoJS.AES.encrypt(data, passphrase).toString();
+    data = encryptedPayload;
+    return data;
+  }
+
   static async ping(): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/`;
     return axios.get(url);
   }
-  static async createBudget(data: { data: string }): Promise<AxiosResponse> {
+
+  static async createBudget(
+    newBudget: Omit<Budget, "id">,
+    passphrase: string = "super secret phrase"
+  ): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets`;
-    const symmetricKey = "secret key";
-    const encryptedPayload = CryptoJS.AES.encrypt(
-      JSON.stringify(data.data),
-      symmetricKey
-    ).toString();
-    data.data = encryptedPayload;
+    const encryptedData: string = this.encryptBudgetData(
+      JSON.stringify(newBudget.data),
+      passphrase
+    );
+    const data: Omit<EncryptedBudget, "id"> = { data: encryptedData };
     return axios.post(url, data);
   }
-  static async getBudgetById(id: string): Promise<AxiosResponse> {
+
+  static async getBudgetById(
+    id: string,
+    passphrase = "super secret phrase"
+  ): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets/${id}`;
     return axios.get(url).then((r) => {
-      const symmetricKey = "secret key";
-      const decrypted = CryptoJS.AES.decrypt(r.data.data, symmetricKey);
-      const decryptedPayload = decrypted.toString(CryptoJS.enc.Utf8);
-      r.data.data = JSON.parse(decryptedPayload);
+      const encryptedBudget: EncryptedBudget = r.data;
+      const decryptor: CryptoJS.lib.WordArray = CryptoJS.AES.decrypt(
+        encryptedBudget.data,
+        passphrase
+      );
+      const decrypted: string = decryptor.toString(CryptoJS.enc.Utf8);
+      r.data.data = JSON.parse(decrypted);
       return r;
     });
   }
-  static async updateBudgetById(id: string, data: any): Promise<AxiosResponse> {
+
+  static async updateBudgetById(
+    id: string,
+    budget: Budget,
+    passphrase: string = "super secret phrase"
+  ): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets/${id}`;
-    return axios.put(url, data);
+    const encryptor: string = this.encryptBudgetData(
+      JSON.stringify(budget.data),
+      passphrase
+    );
+
+    const encryptedBudget: EncryptedBudget = { id: id, data: encryptor };
+    return axios.put(url, encryptedBudget);
   }
 }
