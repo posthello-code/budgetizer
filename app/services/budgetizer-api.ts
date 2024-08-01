baa90869f9550ecab2f127c255fa86398f4b19a0
posthello-code <github@alexjensen.net>
2024-08-01 00:28:38 -0700

    add themis-wasm to backend api

--- app/services/budgetizer-api.ts
+++ app/services/budgetizer-api.ts
@@ -1,15 +1,24 @@
 import axios, { AxiosResponse } from "axios";
 import environment from "../environment/default";
-import CryptoJS from "crypto-js";
 import { Budget, EncryptedBudget } from "../budgets/models";
+import themis, { initialize } from "wasm-themis";
+
 export default class budgetizerApi {
-  private static encryptBudgetData(
+  private static async initThemis() {
+    await initialize("_next/static/chunks/app/libthemis.wasm")
+      .then(() => {})
+      .catch((err) => {});
+  }
+  private static async encryptBudgetData(
     data: string,
     passphrase = "super secret phrase"
   ) {
-    const encryptedPayload = CryptoJS.AES.encrypt(data, passphrase).toString();
-    data = encryptedPayload;
-    return data;
+    // web assembly must be loaded before using themis
+    await this.initThemis();
+    const cell = themis.SecureCellSeal.withPassphrase(passphrase);
+    const plaintext = new TextEncoder().encode(data);
+    const encrypted = cell.encrypt(plaintext);
+    return encrypted;
   }
 
   static async ping(): Promise<AxiosResponse> {
@@ -22,10 +31,11 @@ export default class budgetizerApi {
     passphrase: string = "super secret phrase"
   ): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets`;
-    const encryptedData: string = this.encryptBudgetData(
+    const encryptedData: Uint8Array = await this.encryptBudgetData(
       JSON.stringify(newBudget.data),
       passphrase
     );
+
     const data: Omit<EncryptedBudget, "id"> = { data: encryptedData };
     return axios.post(url, data);
   }
@@ -35,16 +45,25 @@ export default class budgetizerApi {
     passphrase = "super secret phrase"
   ): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets/${id}`;
-    return axios.get(url).then((r) => {
-      const encryptedBudget: EncryptedBudget = r.data;
-      const decryptor: CryptoJS.lib.WordArray = CryptoJS.AES.decrypt(
-        encryptedBudget.data,
-        passphrase
-      );
-      const decrypted: string = decryptor.toString(CryptoJS.enc.Utf8);
-      r.data.data = JSON.parse(decrypted);
-      return r;
-    });
+    return axios
+      .get(url)
+      .then(async (response) => {
+        // web assembly must be loaded before using themis
+        await this.initThemis();
+
+        const arr = new Uint8Array(
+          Object.values(response.data.data) as Array<number>
+        );
+        const cell = themis.SecureCellSeal.withPassphrase(passphrase);
+        const decryptor = cell.decrypt(arr);
+        const decrypted: string = new TextDecoder().decode(decryptor);
+
+        response.data.data = JSON.parse(decrypted);
+        return response;
+      })
+      .catch((e) => {
+        return e;
+      });
   }
 
   static async updateBudgetById(
@@ -53,7 +72,7 @@ export default class budgetizerApi {
     passphrase: string = "super secret phrase"
   ): Promise<AxiosResponse> {
     const url = `${environment.baseUrl}/budgets/${id}`;
-    const encryptor: string = this.encryptBudgetData(
+    const encryptor: Uint8Array = await this.encryptBudgetData(
       JSON.stringify(budget.data),
       passphrase
     );
