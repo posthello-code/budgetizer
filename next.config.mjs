32d2f5c0929febd1acda90219fb795fa1648ce75
posthello-code <github@alexjensen.net>
2024-08-01 00:23:39 -0700

    next config for webpack and themis web assembly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,46 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import path from "path";
+import fs from "fs";
+
+const CWD_PATH = fs.realpathSync(process.cwd());
+
+const nextConfig = {
+  webpack: (config) => {
+    if (!config.plugins) {
+      config.plugins = [];
+    }
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.join(
+              CWD_PATH,
+              "node_modules",
+              "wasm-themis/src/libthemis.wasm"
+            ),
+            to: "static/chunks/app",
+          },
+        ],
+      })
+    );
+    config.resolve.fallback = {
+      crypto: false,
+      fs: false,
+      http: false,
+      https: false,
+      net: false,
+      path: false,
+      stream: false,
+      tls: false,
+      util: false,
+      url: false,
+      zlib: false,
+      ws: false,
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
