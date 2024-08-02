3c71993a0bae01856f66a86fc71be88979c6fd40
posthello-code <github@alexjensen.net>
2024-08-02 14:22:53 -0700

    immutable swr so page doesn't continually reload

--- app/budgets/[id]/swr.ts
+++ app/budgets/[id]/swr.ts
@@ -1,14 +1,17 @@
 import { AxiosResponse } from "axios";
-import useSWR from "swr";
 import budgetizerApi from "../../services/budgetizer-api";
+import useSWRImmutable from "swr/immutable";
 
 const fetcher = async (id: string): Promise<AxiosResponse> => {
-  const r = await budgetizerApi.getBudgetById(id);
+  const item = localStorage.getItem("tempKey") as string;
+  const r = await budgetizerApi.getBudgetById(id, item);
+  localStorage.removeItem("tempKey");
   r.data = { id: r.data._id, data: r.data.data };
   return r.data;
 };
 
 export default function useBudget(id: String) {
-  const { data, error, isLoading } = useSWR(id, fetcher, {});
+  console.log("useBudget");
+  const { data, error, isLoading } = useSWRImmutable(id, fetcher, {});
   return { data, isLoading, isError: error };
 }
